57d38132838f5118089eec861f9b9ce15c394373
Charles Bonnissent <charles.bonnissent@anakeen.com>
2016-03-17 11:33:26 +0100

    feat(autoloader): disable autoload during build css (refs #6320)

--- src/LESS_PHP/Class.dcpLessParser.php
+++ src/LESS_PHP/Class.dcpLessParser.php
@@ -11,8 +11,8 @@ class dcpLessParser implements ICssParser
 
     /**
      * @param string|string[] $srcFiles path or array of path of source file(s) relative to server root
-     * @param array $options array of options
-     * @param array $styleConfig full style configuration
+     * @param array           $options array of options
+     * @param array           $styleConfig full style configuration
      */
     public function __construct($srcFiles, Array $options, Array $styleConfig)
     {
@@ -31,28 +31,65 @@ class dcpLessParser implements ICssParser
 
     /**
      * @param string $destFile destination file path relative to server root (if null, parsed result is returned)
+     *
      * @throws Exception
      * @return mixed
      */
     public function gen($destFile = null)
     {
-        $pubDir = \ApplicationParameterManager::getScopedParameterValue("CORE_PUBDIR", DEFAULT_PUBDIR);
-        $fullTargetPath = $pubDir . DIRECTORY_SEPARATOR . $destFile;
-        $fullTargetDirname = dirname($fullTargetPath);
-        if (!is_dir($fullTargetDirname) && (false === mkdir($fullTargetDirname, 0777, true))) {
-            throw new Exception("STY0005", "$fullTargetDirname dir could not be created for file $destFile");
-        }
-        $parser = new \Less_Parser($this->_options);
-        if (isset($this->_styleConfig["sty_const"]["less_var"])) {
-            $parser->ModifyVars($this->_styleConfig["sty_const"]["less_var"]);
-        }
-        foreach($this->_srcFiles as $srcPath) {
-            $srcFullPath = $pubDir . DIRECTORY_SEPARATOR . $srcPath;
-            $parser->parseFile($srcFullPath);
-        }
-        $css = $parser->getCss();
-        if (false === file_put_contents($fullTargetPath, $css)) {
-                throw new Exception("STY0005", "$fullTargetPath could not be written for file $destFile");
+        $disableAutoload = false;
+        try {
+            $pubDir = \ApplicationParameterManager::getScopedParameterValue(
+                "CORE_PUBDIR", DEFAULT_PUBDIR
+            );
+            $fullTargetPath = $pubDir . DIRECTORY_SEPARATOR . $destFile;
+            $fullTargetDirname = dirname($fullTargetPath);
+            if (!is_dir($fullTargetDirname)
+                && (false === mkdir(
+                        $fullTargetDirname, 0777, true
+                    ))
+            ) {
+                throw new Exception(
+                    "STY0005",
+                    "$fullTargetDirname dir could not be created for file $destFile"
+                );
+            }
+            $parser = new \Less_Parser($this->_options);
+            $autoloadFuncs = spl_autoload_functions();
+
+            foreach ($autoloadFuncs as $unregisterFunc) {
+                spl_autoload_unregister($unregisterFunc);
+            }
+            $disableAutoload = true;
+            if (isset($this->_styleConfig["sty_const"]["less_var"])) {
+                $parser->ModifyVars(
+                    $this->_styleConfig["sty_const"]["less_var"]
+                );
+            }
+            foreach ($this->_srcFiles as $srcPath) {
+                $srcFullPath = $pubDir . DIRECTORY_SEPARATOR . $srcPath;
+                $parser->parseFile($srcFullPath);
+            }
+            $css = $parser->getCss();
+            if (false === file_put_contents($fullTargetPath, $css)) {
+                throw new Exception(
+                    "STY0005",
+                    "$fullTargetPath could not be written for file $destFile"
+                );
+            }
+            if ($disableAutoload) {
+                foreach ($autoloadFuncs as $registerFunc) {
+                    spl_autoload_register($registerFunc);
+                }
+                $disableAutoload = false;
+            }
+        } catch (Exception $e) {
+            if ($disableAutoload) {
+                foreach ($autoloadFuncs as $registerFunc) {
+                    spl_autoload_register($registerFunc);
+                }
+            }
+            throw $e;
         }
     }
 }
